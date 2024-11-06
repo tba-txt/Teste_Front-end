c2f5f8df7848dd055e1d5d39af8801ae4fa3128e
gabriel-txt <gabriel.santana41@fatec.sp.gov.br>
2024-11-06 10:36:15 -0300

    front

---
+++ Index.js
@@ -0,0 +1,32 @@
+
+window.sr = ScrollReveal({ reset:true });
+
+sr. reveal('.navbar_conteudo', { durantion: 3000});
+
+sr. reveal('.navbar', { durantion: 3000});
+
+sr.reveal('.container1', { duration: 3000 });
+
+sr.reveal('.header', { duration: 3000 });
+
+sr.reveal('.container', { duration: 1000 });
+
+sr.reveal('.meio_pagina', { duration: 3000 });
+
+
+sr.reveal('.meio_pagina1', { duration: 1000 });
+
+sr.reveal('.card-meio', { duration: 1000 });
+
+sr.reveal('.meio_pagina2', { duration: 1000 });
+
+sr.reveal('.beneficio-item', {
+    rotate: { x: 0, y:80, z:0},
+    duration: 3000
+});
+sr.reveal('.espaco-avaliacao', { duration: 3000 });
+
+sr.reveal('.avaliacao', { duration: 3000 });
+
+sr. reveal('.footer', { durantion: 3000});
+
